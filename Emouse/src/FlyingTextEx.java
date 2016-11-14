d83d6722c5f4c60ca28a0b401850169ee14f1485
Administrator <Administrator@305-06>
2016-11-14 12:03:43 +0900

    Event processing through Java code

---
+++ Emouse/src/FlyingTextEx.java
@@ -0,0 +1,60 @@
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class FlyingTextEx extends JFrame {
+	JPanel contentPane = new JPanel();
+	JLabel la = new JLabel("hello");
+	final int FLYING_UNIT = 10;
+
+	FlyingTextEx() {
+		setTitle("상,하,좌,우 키를 이용해서 움직여 보세요");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.addKeyListener(new MyKeyListener());
+		la.setLocation(50,50);
+		la.setSize(100,20);
+		contentPane.add(la);
+		setSize(300,300);
+		setVisible(true);
+
+		contentPane.requestFocus();
+	}
+
+	class MyKeyListener extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			int keyCode = e.getKeyCode();
+
+			switch(keyCode){
+			case KeyEvent.VK_UP:
+				la.setLocation(la.getX(), la.getY()-FLYING_UNIT);
+				contentPane.setBackground(Color.YELLOW);
+				break;
+			case KeyEvent.VK_DOWN:
+				la.setLocation(la.getX(), la.getY()+FLYING_UNIT);
+				contentPane.setBackground(Color.magenta);
+				break;
+			case KeyEvent.VK_RIGHT:
+				la.setLocation(la.getX()+FLYING_UNIT, la.getY());
+				contentPane.setBackground(Color.CYAN);
+				break;
+			case KeyEvent.VK_LEFT:
+				la.setLocation(la.getX()-FLYING_UNIT, la.getY());
+				contentPane.setBackground(Color.ORANGE);
+				break;			
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new FlyingTextEx();
+
+	}
+
+}
